281b5f5cc2559a61d6d3e044f4b8a1ee8c65bbc2
Septicake <septicake056@gmail.com>
2025-12-04 11:59:00 -0500

    Ok, it saves, getting loading on startup to work is the next step

--- forge/src/main/kotlin/io/github/real_septicake/hexxyplanes/forge/ForgeHexxyplanes.kt
+++ forge/src/main/kotlin/io/github/real_septicake/hexxyplanes/forge/ForgeHexxyplanes.kt
@@ -4,9 +4,11 @@ import dev.architectury.platform.forge.EventBuses
 import io.github.real_septicake.hexxyplanes.Hexxyplanes
 import io.github.real_septicake.hexxyplanes.HexxyplanesCommands
 import io.github.real_septicake.hexxyplanes.forge.datagen.ForgeHexxyplanesDatagen
+import net.minecraft.core.registries.Registries
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.RegisterCommandsEvent
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.registries.RegisterEvent
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
 @Mod(Hexxyplanes.MODID)
@@ -17,6 +19,9 @@ class ForgeHexxyplanes {
             addListener(ForgeHexxyplanesClient::init)
             addListener(ForgeHexxyplanesDatagen::init)
             addListener(ForgeHexxyplanesServer::init)
+            addListener { evt: RegisterEvent -> evt.register(Registries.CHUNK_GENERATOR) {
+                it.register(Hexxyplanes.id("void"), VoidChunkGenerator.CODEC)
+            } }
         }
         MinecraftForge.EVENT_BUS.apply {
             addListener { evt: RegisterCommandsEvent -> HexxyplanesCommands.register(evt.dispatcher) }
